9dcd6778a0f5b451bec1c62d354440bada189610
Hendra <teknikhendra@gmail.com>
2022-07-16 13:35:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expense = [
+    {
+      id: "a1",
+      title: "Toilet Paper",
+      amount: 27,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "a2",
+      title: "Papeclip",
+      amount: 2,
+      date: new Date(2021, 5, 11),
+    },
+    {
+      id: "a3",
+      title: "Car Door",
+      amount: 300,
+      date: new Date(2022, 1, 21),
+    },
+    {
+      id: "a4",
+      title: "Tissue",
+      amount: 2,
+      date: new Date(2022, 6, 10),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      ></ExpenseItem>
+      <p>this is paragraf</p>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(data) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={data.date} />
+      <div className="expense-item__description">
+        <h2>{data.title}</h2>
+        <div className="expense-item__price">${data.amount}</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
